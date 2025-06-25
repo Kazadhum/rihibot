fb94951c05bc4f45ce0956c2bc71e0f4967df881
Kazadhum <diogoscsv@gmail.com>
2025-06-25 17:56:38 +0100

    fix(webots): rewriting controllers to be rosservice based #1020

---
+++ rihibot_bringup/src/hand_rgb_data_handler.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+from cv_bridge import CvBridge
+import rospy
+from sensor_msgs.msg import CameraInfo, Image
+from std_msgs.msg import Header
+from webots_ros.srv import get_float, set_int, camera_get_info
+import numpy as np
+
+
+class CameraDataHandler:
+    def __init__(self):
+
+        rospy.wait_for_service(service="/robot/get_basic_time_step")
+        robot_get_basic_time_step_srv = rospy.ServiceProxy(
+            name="/robot/get_basic_time_step", service_class=get_float
+        )
+        rospy.wait_for_service(service="/hand_rgb/enable")
+        hand_rgb_enable_srv = rospy.ServiceProxy(
+            name="/hand_rgb/enable", service_class=set_int
+        )
+        rospy.wait_for_service(service="/hand_rgb/get_info")
+        hand_rgb_get_info_srv = rospy.ServiceProxy(
+            name="/hand_rgb/get_info", service_class=camera_get_info
+        )
+
+        timestep = int(robot_get_basic_time_step_srv().value)
+
+        hand_rgb_enable_srv(value=timestep)
+
+        # Camera Info
+        info_resp = hand_rgb_get_info_srv()
+        cam_w = info_resp.width
+        cam_h = info_resp.height
+        cam_fov = info_resp.Fov
+
+        fx = fy = cam_w / (2.0 * np.tan(cam_fov / 2.0))
+        cx = cam_w / 2.0
+        cy = cam_h / 2.0
+
+        rospy.init_node(name="hand_rgb_data_handler", anonymous=True)
+
+        img_sub = rospy.Subscriber(
+            name="/hand_rgb/image", data_class=Image, callback=self.img_recv
+        )
+        self.image_pub = rospy.Publisher(
+            name="hand_rgb/image_raw", data_class=Image, queue_size=10
+        )
+        self.cam_info_pub = rospy.Publisher(
+            name="hand_rgb/camera_info", data_class=CameraInfo, queue_size=10
+        )
+        self.bridge = CvBridge()
+
+        # Static CameraInfo message
+        cam_info_msg = CameraInfo()
+        cam_info_msg.header = Header()
+        cam_info_msg.header.stamp = rospy.Time.now()
+        cam_info_msg.header.frame_id = "hand_rgb_optical_frame"
+        cam_info_msg.width = cam_w
+        cam_info_msg.height = cam_h
+        cam_info_msg.distortion_model = "plumb_bob"
+        cam_info_msg.D = [0.0, 0.0, 0.0, 0.0, 0.0]
+        cam_info_msg.K = [fx, 0.0, cx, 0.0, fy, cy, 0.0, 0.0, 1.0]
+        cam_info_msg.R = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+        cam_info_msg.P = [fx, 0.0, cx, 0.0, 0.0, fy, cy, 0.0, 0.0, 0.0, 1.0, 0.0]
+        self.cam_info_pub.publish(cam_info_msg)
+
+    def img_recv(self, msg):
+
+        new_msg = msg
+        header = Header()
+        header.stamp = msg.header.stamp
+        header.frame_id = "hand_rgb_optical_frame"
+
+        new_msg.header = header
+
+        self.image_pub.publish(new_msg)
+
+
+
+if __name__ == "__main__":
+    image_handler = CameraDataHandler()
+    rospy.spin()
---
+++ rihibot_bringup/src/imu_data_handler.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+"""imu controller."""
+
+from geometry_msgs.msg import Quaternion, Vector3
+from webots_ros.srv import get_float, set_int
+import rospy
+from sensor_msgs.msg import Imu
+
+
+class ImuDataMerger:
+    def __init__(self):
+        # Rospy Service Proxies
+        rospy.wait_for_service(service="/robot/get_basic_time_step")
+        robot_get_basic_time_step_srv = rospy.ServiceProxy(
+            name="/robot/get_basic_time_step", service_class=get_float
+        )
+        rospy.wait_for_service(service="/accelerometer/enable")
+        accelerometer_enable_srv = rospy.ServiceProxy(
+            name="/accelerometer/enable", service_class=set_int
+        )
+        rospy.wait_for_service(service="/gyro/enable")
+        gyro_enable_srv = rospy.ServiceProxy(name="/gyro/enable", service_class=set_int)
+
+        # get the time step of the current world.
+        timestep: int = int(robot_get_basic_time_step_srv().value)
+
+        # Enable sensors
+        accelerometer_enable_srv(value=timestep)
+        gyro_enable_srv(value=timestep)
+
+        rospy.init_node(name="imu_data_handler", anonymous=True)
+
+        gyro_sub = rospy.Subscriber(
+            name="/gyro/values", data_class=Imu, callback=self.gyro_recv
+        )
+        accel_sub = rospy.Subscriber(
+            name="/accelerometer/values", data_class=Imu, callback=self.accel_recv
+        )
+
+        self.imu_pub = rospy.Publisher(name="imu", data_class=Imu, queue_size=10)
+
+        self.gyro_msg = None
+        self.accel_msg = None
+
+        self.rate = rospy.Rate(hz=200)
+        self.run()
+
+
+    def gyro_recv(self, msg):
+        self.gyro_msg = msg
+
+    def accel_recv(self, msg):
+        self.accel_msg = msg
+
+    def run(self):
+        while not rospy.is_shutdown():
+            imu_msg = Imu()
+            if self.gyro_msg and self.accel_msg:
+                # Header
+                imu_msg.header.stamp = rospy.Time.now()
+                imu_msg.header.frame_id = "accelerometer"
+                # Orientation (ignore)
+                imu_msg.orientation = Quaternion(0, 0, 0, 1)
+                imu_msg.orientation_covariance = [0] * 9
+                imu_msg.orientation_covariance[0] = -1  # Ignore orientation
+                # Linear Acceleration
+                imu_msg.linear_acceleration = self.accel_msg.linear_acceleration
+                # Angular Velocity
+                imu_msg.angular_velocity = self.gyro_msg.angular_velocity
+                self.imu_pub.publish(imu_msg)
+
+            self.rate.sleep()
+
+if __name__=="__main__":
+    merger = ImuDataMerger()
