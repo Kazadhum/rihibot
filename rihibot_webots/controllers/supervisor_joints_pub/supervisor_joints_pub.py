59e0e769e59ac5a8e74497ec0c9482ced6f07463
Kazadhum <diogoscsv@gmail.com>
2025-09-17 23:31:57 +0100

    minor: joint publisher is now working

--- rihibot_webots/controllers/supervisor_joints_pub/supervisor_joints_pub.py
+++ rihibot_webots/controllers/supervisor_joints_pub/supervisor_joints_pub.py
@@ -6,12 +6,16 @@ import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
 from webots_ros.msg import Float64Stamped
+from webots_ros.srv import get_float, get_floatRequest
 from controller import Supervisor
+from robotController import RobotController
 
 print("Hi")
 
 # create the Robot instance.
 supervisor = Supervisor()
+
+controller = RobotController()
 
 rospy.init_node(name="joints_pub", anonymous=True)
 
@@ -29,32 +33,36 @@ joint_names = [
     "wrist_3_joint",
 ]
 
+controller.enable_joint_position_sensors()
+
 print("Waiting....")
 for joint in joint_names:
     rospy.wait_for_message(topic=f"/{joint}_sensor/value", topic_type=Float64Stamped)
 
-print("Yay!")
+joint_pub = rospy.Publisher("/joint_states", JointState, queue_size=1)
 
-# joint_pub = rospy.Publisher("/joint_states", JointState, queue_size=10)
+# Set low velocities for joints
+vel_arr = [0.1] * len(controller.joint_names)
+controller.set_joint_velocities(vel_arr)
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while supervisor.step(timestep) != -1 and not rospy.is_shutdown():
     
-#     # # Publish joint states
-#     # joint_msg = JointState()
-#     # joint_msg.header = Header()
+    # Publish joint states
+    joint_msg = JointState()
+    joint_msg.header = Header()
     
-#     # sim_time = supervisor.getTime()
-#     # secs = int(sim_time)
-#     # nsecs = int((sim_time - secs) * 1e9)
+    sim_time = supervisor.getTime()
+    secs = int(sim_time)
+    nsecs = int((sim_time - secs) * 1e9)
     
-#     # joint_msg.header.stamp.secs = secs
-#     # joint_msg.header.stamp.nsecs = nsecs
-#     # joint_msg.name = joint_names
-#     # joint_msg.position = [joint_position_dict[joint] for joint in joint_names]
+    joint_msg.header.stamp.secs = secs
+    joint_msg.header.stamp.nsecs = nsecs
+    joint_msg.name = joint_names
+    joint_msg.position = [controller.joint_position_dict[joint] for joint in joint_names]
     
-#     # joint_pub.publish(joint_msg)
+    joint_pub.publish(joint_msg)
 
 # # Enter here exit cleanup code.
     pass
